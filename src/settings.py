287b23f77e35d4c33f19cf5d5d1d035348b98f88
erastov-alex <erastov.alex@gmail.com>
2023-10-24 07:27:25 +0300

    new preview img

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/syednahinhossain/bangladeshi-license-plate-recognition-dataset/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 7909518
+PREVIEW_IMAGE_ID: int = 7914821
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/bangladeshi-license-plate-recognition-character"
